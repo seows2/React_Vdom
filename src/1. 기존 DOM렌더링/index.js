9f77eaa7c33fd8c57fe2e2e5c331c6c6281434f6
seows2 <seows2@naver.com>
2022-01-15 21:53:51 +0900

    변경을 눈에띄게 하기 위해 setTimeout 적용

--- src/1. 기존 DOM렌더링/index.js
+++ src/1. 기존 DOM렌더링/index.js
@@ -3,5 +3,7 @@ const title = document.createElement("h1"); // (2)
 document.body.appendChild(title); // (3)
 title.textContent = data; // (4)
 
-data = "안녕하세요"; // (1)
-title.textContent = data; // (2)
+setTimeout(() => {
+  data = "안녕하세요"; // (1)
+  title.textContent = data; // (2)
+}, 2000);
